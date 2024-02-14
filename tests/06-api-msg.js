771a081cd1fafb8775cceb3e6b8bd50edfb0de56
Naufal A <spealdm6@gmail.com>
2024-02-14 14:27:53 +0700

    feat: error check and parsed body

--- tests/06-api-msg.js
+++ tests/06-api-msg.js
@@ -1,10 +1,16 @@
 import http from "k6/http";
 import { check } from "k6";
+import { Rate } from "k6/metrics";
 
 export let options = {
   vus: 5,
   duration: "2s",
+  thresholds: {
+    errors: ["rate<0.1"],
+  },
 };
+
+export let errorRate = new Rate("errors");
 
 export default function () {
   let res = http.get(
@@ -15,8 +21,22 @@ export default function () {
     `response body length ${res.body.length} for VU= ${__VU} ITER= ${__ITER}`
   );
 
-  check(res, {
+  let parsedBody = JSON.parse(res.body);
+
+  let chckCode = check(res, {
     "response code was 200": (res) => res.status == 200,
+  });
+
+  let chckBody = check(res, {
     "body size was 43 bytes": (res) => res.body.length == 43,
   });
+
+  let chckMsg = check(parsedBody, {
+    "message in body was API Executed Successfully.": (body) =>
+      body.message == "API Executed Successfully.",
+  });
+
+  errorRate.add(!chckCode);
+  errorRate.add(!chckBody);
+  errorRate.add(!chckMsg);
 }
