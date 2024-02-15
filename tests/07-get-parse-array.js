6b154e5af9e98c5963db5de1980e3afa17fc19f7
Naufal A <spealdm6@gmail.com>
2024-02-15 23:04:14 +0700

    feat: parse array json

---
+++ tests/07-get-parse-array.js
@@ -0,0 +1,24 @@
+import http from "k6/http";
+import { check } from "k6";
+
+export let options = {
+  vus: 5,
+  duration: "2s",
+};
+
+export default function () {
+  var url = "https://run.mocky.io/v3/28a1b472-74be-410c-b740-1c8a14da8998";
+  var headerParam = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  let res = http.get(url, headerParam);
+
+  let parsedBody = JSON.parse(res.body);
+
+  parsedBody.forEach((element) => {
+    console.log(`name is ${element.name}`);
+  });
+}
